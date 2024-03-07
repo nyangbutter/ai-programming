c12e5bdc3384f5808b8ca427570655aa762a6d3b
nyangbutter <lhl0328@naver.com>
2024-03-07 17:30:18 +0900

    first commit

---
+++ print_hello_world_2.py
@@ -0,0 +1,24 @@
+#문자열과 숫자 출력하기
+print('Hello world!')
+print('Hello' , 'world!')
+print('Hello'+'world!')
+print(100)
+print(2+4)
+print(100)
+print(10+20)
+print(10-20)
+print(10*20)
+print(10/20)
+print(123*8765)
+print(10+20*30)
+
+
+#변수 선언하기와 연산하기
+x=100
+y=200
+print(x+y)
+print(x*y)
+print(x/y)
+print(x**2)
+n=5
+print(n**2)
